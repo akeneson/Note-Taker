85460ae85cf32eb1e0f276ede84163823548d76c
Amy Keneson <AmyKeneson@amys-imac.attlocal.net>
2020-09-14 12:19:58 -0500

    routes added

--- server.js
+++ server.js
@@ -7,7 +7,10 @@
 
 // dependencies
 const http = require("http");
-const fs = require ("fs");
+const fs = require("fs");
+var express = require("express");
+
+var app = express();
 
 // set port to 8000
 const PORT = 8080;
@@ -15,3 +18,23 @@ const PORT = 8080;
 // create server
 const server = http.createServer(handleRequest);
 
+// handelRequest function
+function handleRequest(req, res) {
+    console.log("handleRquest function called");
+}
+
+// ============ROUTES=========
+// referenced: Module 14 in express
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get ("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+// ============Listener==========
+// starts server
+server.listen(PORT, function() {
+    console.log("Server is listening on Por: " + PORT);
+});
