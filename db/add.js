bff4bc0ae8934a9501510d1ee47049315aa70368
Amy Keneson <amy.l.keneson@icloud.com>
2020-09-17 18:09:11 -0500

    unable to properly assign ID for deletion

--- db/add.js
+++ db/add.js
@@ -9,6 +9,7 @@ const { v4: uuidv4 } = require('uuid');
 const asyncReadFile = util.promisify(fs.readFile);
 const asyncWriteFile = util.promisify(fs.writeFile);
 
+
 class AddDB {
     readFileSystem() {
         return asyncReadFile("db/db.json", "utf8")
@@ -24,24 +25,31 @@ class AddDB {
             return [].concat(JSON.parse(data))
         })
     }
+
     writeAllNotes(data) {
         const newNote = {
-            id: id + 1,
+            id: uuidv4(),
             title: data.title,
             text: data.text
         }
         console.log(newNote);
         //Changed from this.readFileSystem to readAllNotes so you have an array to spread the data
-        return this.readAllNotes().then(allNotes => [...allNotes, newNote]).then(notes => this.writeFileSystem(notes)).then(() => this.readFileSystem())
-        // first: read our file
-        // second: create a temporary file
-        // third: pushing the new note to allNotes
-        // fourth: after we read the file, we write the previous file with our new file (override)
-        // fifth: rereading allNotes again to display our new note on the page.
+        return this.readAllNotes().then(allNotes => [...allNotes, newNote]).then(notes => this.writeFileSystem(notes)).then(() => this.readFileSystem().then())
+        
+    // writeAllNotes(data) {
+       
+    //     console.log(data);
+    //     //Changed from this.readFileSystem to readAllNotes so you have an array to spread the data
+    //     return this.readAllNotes().then(allNotes => [...allNotes, newNote]).then(notes => this.writeFileSystem(notes)).then(() => this.readFileSystem())
+    //     // first: read our file
+    //     // second: create a temporary file
+    //     // third: pushing the new note to allNotes
+    //     // fourth: after we read the file, we write the previous file with our new file (override)
+    //     // fifth: rereading allNotes again to display our new note on the page.
     }
 
-    deleteNote(id) {
-        return this.readFileSystem().then(notes => { notes.filter(note => note.id !== id) }).then(notes => this.writeFileSystem(notes)).then(() => this.readFileSystem())
+    deleteNote(data) {
+        return this.readFileSystem().then(notes => { notes.indexOf(note => note.id !== id) }).then(notes => this.writeFileSystem(notes)).then(() => this.readFileSystem())
     }
 }
 
